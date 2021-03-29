772e8255812986c2f4224b66dead3e30c5ded027
Fahim Sadman <fahimsadman4@gmail.com>
2021-03-30 01:11:58 +0600

    Updated

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,42 +1,43 @@
-const express = require ('express');
+const express = require("express");
 const router = express.Router();
+const multer = require('multer');
+const checkAuth = require('../middleware/check-auth');
+const ProductsController = require('../controllers/products');
 
-router.get('/', (req, res, next) =>{
-    res.status(200).json({
-        message: 'Handling GET requests to /products'
-    })
-});
-
-router.post('/', (req, res, next) =>{
-    res.status(201).json({
-        message: 'Handling POST requests to /products'
-    });
-});
-
-router.get('/:productID', (req, res, next) => {
-    const id = req.params.productId;
-    if (id === 'special') {
-        res.status(200).json({
-            message: 'You discovered the special ID',
-            id: id
-        });
-    } else {
-        res.status(200).json({
-            message:'You passed an ID'
-        });
-    }
+const storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+    cb(null, './uploads/');
+  },
+  filename: function(req, file, cb) {
+    cb(null, new Date().toISOString() + file.originalname);
+  }
 });
 
-router.patch('/:productID', (req, res, next) => {
-    res.status(200).json({
-        message: 'Updated product!'
-    });
+const fileFilter = (req, file, cb) => {
+  // reject a file
+  if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 1024 * 1024 * 5
+  },
+  fileFilter: fileFilter
 });
-    
-router.delete('/:productID', (req, res, next) => {
-    res.status(200).json({
-        message: 'Deleted product!'
-    });
-});
+
+router.get("/", ProductsController.products_get_all);
+
+router.post("/", checkAuth, upload.single('productImage'), ProductsController.products_create_product);
+
+router.get("/:productId", ProductsController.products_get_product);
+
+router.patch("/:productId", checkAuth, ProductsController.products_update_product);
+
+router.delete("/:productId", checkAuth, ProductsController.products_delete);
 
 module.exports = router;
